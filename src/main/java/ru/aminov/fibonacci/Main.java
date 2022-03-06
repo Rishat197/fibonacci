8db4328de504ac032443459f3a9dc559356b6a67
Rishat Aminov <rishat71@rambler.ru>
2022-03-06 22:42:30 +0500

    Изменение Main

--- src/main/java/ru/aminov/fibonacci/Main.java
+++ src/main/java/ru/aminov/fibonacci/Main.java
@@ -1,7 +1,28 @@
 package ru.aminov.fibonacci;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        boolean choice = true;
+        while (choice) {
+            System.out.println("Введите номер числа Фибоначчи, который хотите узнать:");
+            int number = input.nextInt();
+            System.out.println(number + "-е число Фибоначчи равно: " + fib(number));
+            System.out.println("Нажмите клавишу \"y\", если хотите выйти из программы");
+            String character = input.next();
+            if (character.equals("y")) {
+                break;
+            }
+        }
     }
+
+    static int fib(int num) {
+        if (num <= 1) {
+            return num;
+        } else {
+            return fib(num - 1) + fib(num - 2);
+        }
+    }
+
 }
